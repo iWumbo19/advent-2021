781486defed3a8b273fccc3b8ff8b4c244d1b6d5
iWumbo19 <rancourtvideo@gmail.com>
2021-12-02 20:05:47 -0500

    Add project files.

---
+++ advent-day1/Day1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace advent_day2
+{
+    class Day2
+    {
+        static void Main(string[] args)
+        {
+            int increases = 0;
+            int lastSum = 0;
+            int[] altitudes = new int[2000];
+
+
+
+
+            int lineCount = 0;
+            foreach (string line in System.IO.File.ReadLines(@"day1-input.txt"))
+            {
+                altitudes[lineCount] = int.Parse(line);
+                lineCount++;
+            }
+
+
+
+            for (int i = 0; i < 1998; i++)
+            {
+                int a = altitudes[i];
+                int b = altitudes[i + 1];
+                int c = altitudes[i + 2];
+                int sum = a + b + c;
+                if (lastSum != 0)
+                {
+                    if (sum > lastSum)
+                    {
+                        increases++;
+                        lastSum = sum;
+                    }
+                    else { lastSum = sum; }
+                }
+                else { lastSum = sum; }
+            }
+
+            Console.WriteLine(increases);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ advent-day2/Day2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace advent_day2
+{
+    class Day2
+    {
+        static void Main(string[] args)
+        {
+            int horz = 0;
+            int depth = 0;
+            int aim = 0;
+            foreach (string line in System.IO.File.ReadLines(@"day2-input.txt"))
+            {
+                
+                string[] instructions = line.Split(' ');
+                switch (instructions[0])
+                {
+                    case "forward":
+                        horz += int.Parse(instructions[1]);
+                        depth += aim * int.Parse(instructions[1]);
+                        break;
+                    case "down":
+                        aim += int.Parse(instructions[1]);
+                        break;
+                    case "up":
+                        aim -= int.Parse(instructions[1]);
+                        break;
+                    default:
+                        break;
+                } 
+            }
+            Console.WriteLine(horz * depth);
+        }
+    }
+}
