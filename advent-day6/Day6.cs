fe9d38aeb212c41f4b7a09dedbd6307e3c1f095a
Chris Rancourt <rancourtvideo@gmail.com>
2021-12-07 15:04:17 -0500

    Day 6 So close to part2. Need to make more memory efficient. Categorize by birthday?

---
+++ advent-day6/Day6.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace advent_day6
+{
+    class Day6
+    {
+        static void Main(string[] args)
+        {
+            List<int> initialFish = new List<int>();
+            string firstLine = System.IO.File.ReadLines("day6-input").First();
+            string[] fish = firstLine.Split(',');
+            foreach (var item in fish)
+            {
+                initialFish.Add(int.Parse(item));
+            }
+            for (int days = 0; days < 80; days++)
+            {
+                int fishNeeded = 0;
+                for (int fishIndex = 0; fishIndex < initialFish.Count; fishIndex++)
+                {
+                    if (initialFish[fishIndex] == 0)
+                    {
+                        initialFish[fishIndex] = 6;
+                        fishNeeded++;
+                    }
+                    else { initialFish[fishIndex]--; }
+                }
+                for (int newFish = 0; newFish < fishNeeded; newFish++)
+                {
+                    initialFish.Add(8);
+                }
+                Console.WriteLine($"Day {days}: {initialFish.Count}");
+            }
+            Console.WriteLine($"Total Fish {initialFish.Count}");
+
+
+        }
+    }
+
+}
