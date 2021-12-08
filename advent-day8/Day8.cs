811136156bb9f4306dfdbcc4e0c748f751d8696c
Chris Rancourt <rancourtvideo@gmail.com>
2021-12-08 09:27:56 -0500

    Day 8 Part one success. Part 2 is doable

---
+++ advent-day8/Day8.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace advent_day8
+{
+    class Day8
+    {
+        static void Main(string[] args)
+        {
+            List<string> initialSplit = new List<string>();
+            foreach (string line in System.IO.File.ReadLines("day8-input")) { initialSplit.Add(line.Split('|')[1]); }
+            //foreach (string item in initialSplit) { Console.WriteLine(item); }
+            int counter = 0;
+            foreach (string item in initialSplit)
+            {
+                foreach (string digit in item.Split())
+                {
+                    if (digit.Length == 2 ^
+                        digit.Length == 4 ^
+                        digit.Length == 3 ^ 
+                        digit.Length == 7)
+                    {
+                        counter++;
+                    }
+                }
+            }
+            Console.WriteLine(counter);         
+
+        }
+    }
+}
