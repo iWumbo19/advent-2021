6b3ac9d66209968e619eb20cb5086697277d72eb
Chris Rancourt <rancourtvideo@gmail.com>
2021-12-10 10:50:04 -0500

    Day 10 part 2 failing. Dont know why

--- advent-day10/Day10.cs
+++ advent-day10/Day10.cs
@@ -11,6 +11,7 @@ namespace advent_day10
         public static Dictionary<char, int> CharValues = new Dictionary<char, int>() { { ')', 3 }, { ']', 57 }, { '}', 1197 }, { '>', 25137 } };
         public static Dictionary<char,char> Counterpart = new Dictionary<char, char>() { { '(', ')' }, { '[', ']' }, { '{', '}' }, { '<', '>' } };
         public static List<string> IncompleteLines = new List<string>();
+        public static List<int> LineErrorScores = new List<int>();
 
         public static List<char> IllegalChars = new List<char>();
         public static int TotalIllegalValue = 0;
@@ -34,19 +35,65 @@ namespace advent_day10
                         }
                         else
                         {
-                            TotalIllegalValue += CharValues[symbol];
-                            Console.WriteLine($"Expected {tracker.Peek()}: Found {symbol}: line {lineCounter}");
                             break;
                         }
                     }
                     lineCounter++;
                 }
+                Console.WriteLine($"Adding Line {IncompleteLines.Count}");
                 IncompleteLines.Add(line);
             }
 
+            foreach (string line in IncompleteLines)
+            {
+                Stack<char> tracker = new Stack<char>();
+                foreach (char symbol in line)
+                {
+                    if (Openers.Contains(symbol))
+                    {
+                        tracker.Push(symbol);
+                    }
+                    else
+                    {
+                        tracker.Pop();
+                    }
+                    lineCounter++;
+                }
 
+                Console.WriteLine($"Adding {tracker.Count} to Value");
+                int stackSize = tracker.Count;
+                TotalIllegalValue = 0;
+                while (tracker.Count > 0)
+                {
+                    char adder = tracker.Pop();
+                    if (adder == '(')
+                    {
+                        TotalIllegalValue += TotalIllegalValue * 5;
+                        TotalIllegalValue += 1;
+                    }
+                    else if (adder == '[')
+                    {
+                        TotalIllegalValue += TotalIllegalValue * 5;
+                        TotalIllegalValue += 2;
+                    }
+                    else if (adder == '{')
+                    {
+                        TotalIllegalValue += TotalIllegalValue * 5;
+                        TotalIllegalValue += 3;
+                    }
+                    else if (adder == '<')
+                    {
+                        TotalIllegalValue += TotalIllegalValue * 5;
+                        TotalIllegalValue += 4;
+                    }
+                }
+                LineErrorScores.Add(TotalIllegalValue);
+            }
+            LineErrorScores.Sort();
+
+            decimal middle = Math.Round((decimal)LineErrorScores.Count / 2);
             
-            Console.WriteLine($"Total Illegal Value: {TotalIllegalValue}");
+            Console.WriteLine($"{LineErrorScores[(int)middle]}");
 
         }
     }
